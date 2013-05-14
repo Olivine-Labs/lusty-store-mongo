593d322532876a48090fd32d56d3c571a8457083
Drew Ditthardt <dditthardt@olivinelabs.com>
2013-05-13 20:56:23 -0700

    allow return values

--- src/store/mongo.lua
+++ src/store/mongo.lua
@@ -2,5 +2,5 @@ local util = require 'lusty.util'
 
 return {
   handler = function(context)
-    util.inline((...)..'.'..string.lower(context.request.method), {context=context, config=config})
+    return util.inline((...)..'.'..string.lower(context.request.method), {context=context, config=config})
   end
