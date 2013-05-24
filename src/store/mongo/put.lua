54aa087197fc48ab584911dd7bbfef26191f146b
Drew Ditthardt <dditthardt@olivinelabs.com>
2013-05-23 21:53:43 -0700

    Fixed bug in put

--- src/store/mongo/put.lua
+++ src/store/mongo/put.lua
@@ -1,9 +1,13 @@
 local util = require 'lusty.util'
 local packageName = (...):match("(.-)[^%.]+$")
+local object_id = require 'resty-mongol.object_id'
 
 return {
   handler = function(context)
     local col = util.inline(packageName..'.connection', {lusty=lusty, config=config})
+    if not context.data['_id'] then
+      context.data['_id'] = object_id.new():tostring()
+    end
     local meta = getmetatable(context.data)
     if meta and type(meta.__toStore) == "function" then
       data = meta.__toStore(data, "put")
