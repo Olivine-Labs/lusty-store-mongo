d855825ee1b454a5777d85a796214ff738a0cf94
Drew Ditthardt <dditthardt@olivinelabs.com>
2013-06-05 12:45:57 -0700

    Made store interface handle find_one cases a bit better

--- src/store/mongo/get.lua
+++ src/store/mongo/get.lua
@@ -4,10 +4,10 @@ local packageName = (...):match("(.-)[^%.]+$")
 return {
   handler = function(context)
     local col = util.inline(packageName..'.connection', {lusty=lusty, config=config})
-    local results = {}
     if context.query['_id'] then
       return col:find_one(context.query, context.data)
     else
+      local results = {}
       local cursor = col:find(context.query, context.data)
       for index, result in cursor:pairs() do
         table.insert(results, result)
