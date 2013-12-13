63a0bb01b31858267e35fba9a6cb801e2cde42aa
Drew Ditthardt <dditthardt@olivinelabs.com>
2013-12-12 17:21:28 -0800

    fixed weird bug in put due to changes

--- src/store/mongo/put.lua
+++ src/store/mongo/put.lua
@@ -8,7 +8,7 @@ return {
     local q = query(context.query)
     local col = util.inline(packageName..'.connection', {lusty=lusty, config=config})
     if not context.data['_id'] then
-      context.data['_id'] = q['_id'] or objectId.new():tostring()
+      context.data['_id'] = q['_id'] and q['_id']['$in'][1] or objectId.new():tostring()
     end
     col:delete(q)
     context.data.lastModified = os.time()
