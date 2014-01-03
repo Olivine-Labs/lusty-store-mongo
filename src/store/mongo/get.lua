ac61388043dd4d2845a7e57205bc73f9b0e10509
Drew Ditthardt <dditthardt@olivinelabs.com>
2014-01-02 18:28:27 -0800

    implemented limit in get

--- src/store/mongo/get.lua
+++ src/store/mongo/get.lua
@@ -4,13 +4,23 @@ local query = require 'lusty-store-mongo.query'
 
 return {
   handler = function(context)
-    local q = query(context.query)
+    local q, m = query(context.query)
     local col = util.inline(packageName..'.connection', {lusty=lusty, config=config})
     if q['_id'] then
       return col:find_one(q, context.data)
     else
       local results = {}
       local cursor = col:find(q, context.data)
+
+      if m.limit then
+        if m.limit.offset then
+          cursor.skip(m.limit.offset)
+        end
+        if m.limit.length then
+          cursor.limit(m.limit.length)
+        end
+      end
+
       for index, result in cursor:pairs() do
         table.insert(results, result)
       end
