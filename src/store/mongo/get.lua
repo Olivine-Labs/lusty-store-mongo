d80ac134fed915e34300992a6050b144bfd2fc03
Drew Ditthardt <dditthardt@olivinelabs.com>
2014-01-07 15:51:12 -0800

    bugfix release for fields

--- src/store/mongo/get.lua
+++ src/store/mongo/get.lua
@@ -6,29 +6,25 @@ return {
   handler = function(context)
     local q, m = query(context.query)
     local col = util.inline(packageName..'.connection', {lusty=lusty, config=config})
-    if q['_id'] then
-      return col:find_one(q, context.data)
-    else
-      local results = {}
-      local cursor = col:find(q, context.data)
+    local results = {}
+    local cursor = col:find(q, m.fields)
 
-      if m.sort then
-        cursor:sort(m.sort)
-      end
-
-      if m.limit then
-        if m.limit.offset then
-          cursor:skip(m.limit.offset)
-        end
-        if m.limit.length then
-          cursor:limit(m.limit.length)
-        end
-      end
-
-      for index, result in cursor:pairs() do
-        table.insert(results, result)
-      end
-      return results
+    if m.sort then
+      cursor:sort(m.sort)
     end
+
+    if m.limit then
+      if m.limit.offset then
+        cursor:skip(m.limit.offset)
+      end
+      if m.limit.length then
+        cursor:limit(m.limit.length)
+      end
+    end
+
+    for index, result in cursor:pairs() do
+      table.insert(results, result)
+    end
+    return results
   end
 }
